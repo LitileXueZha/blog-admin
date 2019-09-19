7893343db6ad2ebdc256adeee7fecf390de1566b
宁涛 <tao.n@shulidata.com>
2019-09-19 18:49:33 +0800

    feat: 初始化

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+ReactDOM.render(
+    <React.Fragment>
+        test
+    </React.Fragment>,
+    document.getElementById('app'),
+);
